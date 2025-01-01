2368c5629bdafda883495a2634cb5e0f20e9ab3a
Michael <80940180+michael-freling@users.noreply.github.com>
2024-12-31 17:59:40 -0800

    backend: run a vaildation of image files concurrently

--- internal/import_images/batch_images.go
+++ internal/import_images/batch_images.go
@@ -8,10 +8,12 @@ import (
 	"log/slog"
 	"os"
 	"path/filepath"
+	"sync"
 	"time"
 
 	"github.com/michael-freling/anime-image-viewer/internal/db"
 	"github.com/michael-freling/anime-image-viewer/internal/image"
+	"golang.org/x/sync/errgroup"
 )
 
 type BatchImageImporter struct {
@@ -35,122 +37,196 @@ func NewBatchImageImporter(
 	}
 }
 
-type ProgressEvent struct {
-	Total        int
-	Completed    int
-	Failed       int
-	FailedPath   []string
-	FailedErrors []error
-}
-
 func (batchImporter *BatchImageImporter) ImportImages(
 	ctx context.Context,
 	destinationParentDirectory image.Directory,
 	paths []string,
-	progressCallback func(ProgressEvent),
+	progressNotifier *ProgressNotifier,
 ) ([]image.ImageFile, error) {
-	imageErrors := make([]error, 0)
-	newImages := make([]db.File, 0)
-	newImagePaths := make([]string, 0)
-
-	failedPaths := make([]string, 0)
-	for _, sourceFilePath := range paths {
-		fileName := filepath.Base(sourceFilePath)
-		pathStat, err := os.Stat(sourceFilePath)
-		if err != nil {
-			imageErrors = append(imageErrors, fmt.Errorf("os.Stat: %w: %s", err, sourceFilePath))
-			failedPaths = append(failedPaths, sourceFilePath)
-			continue
-		}
-		if pathStat.IsDir() {
-			// if it's a directory, import it recursively
-			// todo
-			continue
-		}
-		if err := batchImporter.validateImportImageFile(sourceFilePath, destinationParentDirectory); err != nil {
-			imageErrors = append(imageErrors, err)
-			failedPaths = append(failedPaths, sourceFilePath)
-			continue
-		}
-
-		newImages = append(newImages, db.File{
-			Name:     fileName,
-			ParentID: destinationParentDirectory.ID,
-			Type:     db.FileTypeImage,
-		})
-		newImagePaths = append(newImagePaths, sourceFilePath)
+	validator := newBatchImportImageValidator(
+		batchImporter.dbClient,
+		progressNotifier,
+	)
+	newImages, newImagePaths, err := validator.validateImages(
+		ctx,
+		paths,
+		destinationParentDirectory,
+	)
+	if err != nil {
+		// unexpected error occurred and not to keep running the process
+		return nil, fmt.Errorf("validateImages: %w", err)
 	}
 	batchImporter.logger.DebugContext(ctx, "importImageFiles",
 		"directory", destinationParentDirectory,
 		"paths", paths,
 		"newImages", newImages,
-		"imageErrors", imageErrors,
 	)
 	if len(newImages) == 0 {
-		return nil, errors.Join(imageErrors...)
+		return nil, errors.Join(progressNotifier.FailedErrors...)
 	}
 
 	if err := db.BatchCreate(batchImporter.dbClient, newImages); err != nil {
-		imageErrors = append(imageErrors, fmt.Errorf("BatchCreate: %w", err))
-		return nil, errors.Join(imageErrors...)
+		return nil, errors.Join(
+			fmt.Errorf("BatchCreate: %w", err),
+			errors.Join(progressNotifier.FailedErrors...),
+		)
 	}
 
-	total, completed, failed := len(paths), 0, len(failedPaths)
-	progressCallback(ProgressEvent{
-		Total:        total,
-		Completed:    completed,
-		Failed:       failed,
-		FailedErrors: imageErrors,
-	})
-
-	done := make(chan struct{})
-	go func() {
-		isEnded := false
-		for !isEnded {
-			select {
-			case <-done:
-				isEnded = true
-			case <-time.After(1 * time.Second):
-			}
-			progressCallback(ProgressEvent{
-				Total:        total,
-				Completed:    completed,
-				Failed:       failed,
-				FailedPath:   failedPaths,
-				FailedErrors: imageErrors,
-			})
-		}
-	}()
-	resultImageFiles := make([]image.ImageFile, 0)
+	eg, _ := errgroup.WithContext(ctx)
+	resultImageFiles := make([]image.ImageFile, len(newImages))
 	for index, newImage := range newImages {
 		sourceFilePath := newImagePaths[index]
 		destinationFilePath := filepath.Join(destinationParentDirectory.Path, newImage.Name)
-		if _, err := image.Copy(sourceFilePath, destinationFilePath); err != nil {
-			imageErrors = append(imageErrors, fmt.Errorf("copy: %w", err))
-			failedPaths = append(failedPaths, sourceFilePath)
-			failed++
-			continue
-		}
-		resultImage, err := batchImporter.imageFileConverter.ConvertImageFile(destinationParentDirectory, newImage)
-		if err != nil {
-			imageErrors = append(imageErrors, fmt.Errorf("convertImageFile: %w", err))
-			failedPaths = append(failedPaths, sourceFilePath)
-			failed++
-			continue
-		}
-		resultImageFiles = append(resultImageFiles, resultImage)
-		completed++
 
+		eg.Go(func() error {
+			if _, err := image.Copy(sourceFilePath, destinationFilePath); err != nil {
+				progressNotifier.addFailure(sourceFilePath, fmt.Errorf("image.copy: %w", err))
+				return nil
+			}
+			resultImage, err := batchImporter.imageFileConverter.ConvertImageFile(destinationParentDirectory, newImage)
+			if err != nil {
+				progressNotifier.addFailure(sourceFilePath, fmt.Errorf("convertImageFile: %w", err))
+				return nil
+			}
+
+			resultImageFiles[index] = resultImage
+			progressNotifier.addSuccess()
+			return nil
+		})
 	}
-	close(done)
-	if len(imageErrors) > 0 {
-		return resultImageFiles, errors.Join(imageErrors...)
+	if err := eg.Wait(); err != nil {
+		return nil, errors.Join(
+			fmt.Errorf("errgroup.Wait: %w", err),
+			errors.Join(progressNotifier.FailedErrors...),
+		)
+	}
+	if len(progressNotifier.FailedErrors) > 0 {
+		return resultImageFiles, errors.Join(progressNotifier.FailedErrors...)
 	}
 
 	return resultImageFiles, nil
 }
 
-func (service *BatchImageImporter) validateImportImageFile(
+type ProgressNotifier struct {
+	Completed    int
+	Failed       int
+	FailedPaths  []string
+	FailedErrors []error
+
+	successMutex sync.Mutex
+	failureMutex sync.Mutex
+}
+
+func NewProgressNotifier() *ProgressNotifier {
+	return &ProgressNotifier{
+		FailedPaths:  make([]string, 0),
+		FailedErrors: make([]error, 0),
+	}
+}
+
+func (notifier *ProgressNotifier) addSuccess() {
+	notifier.successMutex.Lock()
+	notifier.Completed++
+	notifier.successMutex.Unlock()
+}
+
+func (notifier *ProgressNotifier) addFailure(path string, err error) {
+	notifier.failureMutex.Lock()
+	notifier.Failed++
+	notifier.FailedPaths = append(notifier.FailedPaths, path)
+	notifier.FailedErrors = append(notifier.FailedErrors, err)
+	notifier.failureMutex.Unlock()
+}
+
+func (notifier *ProgressNotifier) Run(done <-chan struct{}, progressCallback func()) {
+	isEnded := false
+	for !isEnded {
+		select {
+		case <-done:
+			isEnded = true
+		case <-time.After(1 * time.Second):
+		}
+
+		progressCallback()
+	}
+}
+
+type batchImportImageValidator struct {
+	dbClient         *db.Client
+	progressNotifier *ProgressNotifier
+}
+
+func newBatchImportImageValidator(dbClient *db.Client, progressNotifier *ProgressNotifier) batchImportImageValidator {
+	return batchImportImageValidator{
+		dbClient:         dbClient,
+		progressNotifier: progressNotifier,
+	}
+}
+
+func (batchValidator batchImportImageValidator) validateImages(
+	ctx context.Context,
+	paths []string,
+	destinationParentDirectory image.Directory,
+) (
+	[]db.File,
+	[]string,
+	error,
+) {
+	newImages := make([]db.File, len(paths))
+	newImagePaths := make([]string, len(paths))
+
+	eg, _ := errgroup.WithContext(ctx)
+	for index, sourceFilePath := range paths {
+		eg.Go(func() error {
+			fileName := filepath.Base(sourceFilePath)
+			pathStat, err := os.Stat(sourceFilePath)
+			if err != nil {
+				batchValidator.progressNotifier.addFailure(sourceFilePath,
+					fmt.Errorf("os.Stat: %w: %s", err, sourceFilePath),
+				)
+				return nil
+			}
+			if pathStat.IsDir() {
+				// if it's a directory, import it recursively
+				// todo
+				return nil
+			}
+			if err := batchValidator.validateImportImageFile(sourceFilePath, destinationParentDirectory); err != nil {
+				batchValidator.progressNotifier.addFailure(sourceFilePath, err)
+				return nil
+			}
+
+			newImages[index] = db.File{
+				Name:     fileName,
+				ParentID: destinationParentDirectory.ID,
+				Type:     db.FileTypeImage,
+			}
+			newImagePaths[index] = sourceFilePath
+			return nil
+		})
+	}
+	if err := eg.Wait(); err != nil {
+		return nil, nil, errors.Join(
+			fmt.Errorf("errgroup.Wait: %w", err),
+			errors.Join(batchValidator.progressNotifier.FailedErrors...),
+		)
+	}
+
+	// filter failed images
+	resultImages := make([]db.File, 0, len(newImages))
+	resultImagePaths := make([]string, 0, len(newImagePaths))
+	for index := range newImages {
+		if newImagePaths[index] == "" {
+			continue
+		}
+
+		resultImages = append(resultImages, newImages[index])
+		resultImagePaths = append(resultImagePaths, newImagePaths[index])
+	}
+	return resultImages, resultImagePaths, nil
+}
+
+func (validator *batchImportImageValidator) validateImportImageFile(
 	sourceFilePath string,
 	destinationDirectory image.Directory,
 ) error {
@@ -167,7 +243,7 @@ func (service *BatchImageImporter) validateImportImageFile(
 		return fmt.Errorf("os.Stat: %w: %s", err, destinationFilePath)
 	}
 
-	record, err := db.FindByValue(service.dbClient, &db.File{
+	record, err := db.FindByValue(validator.dbClient, &db.File{
 		Name:     fileName,
 		ParentID: destinationDirectory.ID,
 	})
