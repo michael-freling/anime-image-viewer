9094aee25841f124be8c9629575edf4b6e23f23f
Michael <80940180+michael-freling@users.noreply.github.com>
2025-01-12 08:05:43 -0800

    suggestion: update image width to 224

--- plugins/tag-suggestion/src/main.py
+++ plugins/tag-suggestion/src/main.py
@@ -17,10 +17,10 @@ def cli():
 @cli.command('preprocess')
 @click.argument('input_dir', type=click.Path(exists=True))
 @click.argument('output_dir', type=click.Path())
-@click.option('--target-width', default=512, help='Width to resize the images to')
-def preprocess_data(input_dir: str, output_dir: str, target_width: int):
+@click.option('--target-image-width', default=224, help='Width to resize the images to')
+def preprocess_data(input_dir: str, output_dir: str, target_image_width: int):
     preprocessor = preprocess.Preprocessor()
-    preprocessor.process_images(input_dir, output_dir, target_width)
+    preprocessor.process_images(input_dir, output_dir, target_image_width)
 
 
 @cli.command('train', context_settings=dict(ignore_unknown_options=True, allow_extra_args=True))
@@ -36,7 +36,7 @@ def train_model(ctx, input_dir: str):
 @cli.command('predict')
 @click.argument('model_path', type=click.Path(exists=True))
 @click.argument('image_paths', type=click.Path(exists=True), nargs=-1)
-@click.option('--resize-image-width', default=config.DEFAULT_INFERENCE_IMAGE_SIZE, help='Width to resize the input images to')
+@click.option('--resize-image-width', default=224, help='Width to resize the input images to')
 def predict_image(model_path: str, image_paths: list[str], resize_image_width: int):
     inferer = inference.Inference(model_path, resize_image_width)
     print(json.dumps(inferer.predict(image_paths), indent=2))
@@ -44,7 +44,7 @@ def predict_image(model_path: str, image_paths: list[str], resize_image_width: i
 
 @cli.command('server')
 @click.argument('model_path', type=click.Path(exists=True))
-@click.option('--resize-image-width', default=512, help='Width to resize the input images to')
+@click.option('--resize-image-width', default=224, help='Width to resize the input images to')
 def server(model_path: str, resize_image_width: int):
     start_grpc_server(model_path, resize_image_width)
 
